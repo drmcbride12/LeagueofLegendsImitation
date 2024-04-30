98e7d398c97ef8af6756379b869539c5c6c263d3
drmcbride12 <130199368+drmcbride12@users.noreply.github.com>
2024-04-30 18:11:58 -0400

    Create data_collector.py

---
+++ data_collector.py
@@ -0,0 +1,46 @@
+import os
+import pyautogui
+import datetime
+from pynput import mouse, keyboard
+from PIL import Image
+
+# Create a directory for screenshots if it doesn't exist
+screenshot_dir = "screenshots"
+os.makedirs(screenshot_dir, exist_ok=True)
+
+def take_action_screenshot(action, x, y):
+    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S%f")[:-3]  # Milliseconds precision
+    filename = f"{timestamp},{action},{x},{y}.jpg"  # Change file extension to .jpg
+    # Save the screenshot in the screenshots folder
+    filepath = os.path.join(screenshot_dir, filename)
+    screenshot = pyautogui.screenshot()
+    
+    # Compress the image
+    screenshot = screenshot.convert('RGB')
+    screenshot.save(filepath, optimize=True, quality=30)  # Adjust the quality as needed
+    
+    print(f"Saved: {filepath}")
+
+def on_click(x, y, button, pressed):
+    if pressed:
+        action = f"{button} click"
+        take_action_screenshot(action, x, y)
+
+def on_press(key):
+    try:
+        # Get the current mouse position
+        x, y = pyautogui.position()
+        action = f"keypress-{key}"
+        take_action_screenshot(action, x, y)
+    except AttributeError:
+        pass  # Special keys (like ctrl) will throw this error
+
+# Setting up the listeners
+mouse_listener = mouse.Listener(on_click=on_click)
+keyboard_listener = keyboard.Listener(on_press=on_press)
+
+mouse_listener.start()
+keyboard_listener.start()
+
+mouse_listener.join()
+keyboard_listener.join()
